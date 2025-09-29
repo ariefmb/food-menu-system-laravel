bfa88be35aadb3ff1b4aaf866de5aaa946449632
Arief Budiman <mariefbudiman12@gmail.com>
2025-09-29 12:55:16 +0700

    fixing minor bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Food Menu</title>
     @vite('resources/css/app.css')
-    @vite('resources/js/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body class="bg-gray-700 text-gray-100 font-sans">
     <div class="min-h-screen flex flex-col">
